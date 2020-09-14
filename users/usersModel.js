2e217850c9c75ab41030733dd55f8daabb0cb0c7
MMendes24 <mmbusiness1@protonmail.com>
2020-09-14 16:03:17 -0400

    User saves with the password hashed.

--- users/usersModel.js
+++ users/usersModel.js
@@ -2,8 +2,14 @@ const db = require('../data/dbConfig')
 
 module.exports = {
     get,
+    add,
 }
 
 function get() {
     return db('users')
+}
+
+function add(user) {
+    return db('users')
+    .insert(user)
 }
--- users/usersRouter.js
+++ users/usersRouter.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
+const bcryptjs = require("bcryptjs");
 
 const Users = require('./usersModel');
 
-router.get('/', (req, res) => {
+router.get('/users', (req, res) => {
     Users.get()
         .then(users => {
             res.status(200).json(users);
@@ -13,5 +14,37 @@ router.get('/', (req, res) => {
             res.status(500).json({ message: err.message });
         });
 })
+
+router.post('/register', (req, res) => {
+    const user = req.body
+    const valid = validateUser(user)
+
+    if (valid) {
+        const hash = bcryptjs.hashSync(user.password, 8);
+        user.password = hash
+
+        Users.add(user)
+            .then(thenRes => {
+                res.status(201).json({ data: thenRes });
+            })
+            .catch(error => {
+                res.status(500).json({ message: error.message });
+            });
+    } else {
+        res.status(400).json({
+            message: "Invalid information, plese verify and try again",
+        });
+    }
+})
+
+function validateUser(user) {
+    // has username, password and role
+    return user.username && user.password ? true : false;
+}
+
+function validateCredentials(creds) {
+    // has username, password and role
+    return creds.username && creds.password ? true : false;
+}
 
 module.exports = router
